5d1315a0fb85937914d4fb05d5726e03079de606
ZUBUMAFFU <114173832+ZUBUMAFFU@users.noreply.github.com>
2025-06-17 14:29:05 -0300

    Add files via upload

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // <-- adicione isso
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-})
+});
+
 pool.getConnection()
 .then(()=>{
     console.log("conectado ao banco de dados com sucesso")
